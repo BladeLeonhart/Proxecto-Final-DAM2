6df4a6fed19c23403f50f34232db5802108817ff
Blade Leonhart <machosotoivan@gmail.com>
2020-05-07 01:53:17 +0200

    First Commit
    Proxecto engadido ao repositorio

---
+++ Zona de Proba/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void BotonPlay() 
+
+    {
+        //SceneManager.LoadScene("EscogerCoche");
+    }
+
+    public void BotonSelect()
+    { }
+
+    public void BotonQuit()
+    {
+        Debug.Log("Game QUITED!");
+        Application.Quit();
+    }
+
+
+
+
+
+}

8270efa4632b099c6713d2d06177051b8ff9c003
MrLage99 <mrlage99@gmail.com>
2020-05-11 17:53:58 +0200

    commit menu(que faltaba)
    subi el comit con el menu

---
+++ Crazy_Batlekarts/Assets/Scripts/KartController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartController : MonoBehaviour
+{
+
+
+
+
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+
+
+    float speed, currentSpeed;
+    float rotate, currentRotate;
+    int driftDirection;
+    float driftPower;
+    int driftMode = 0;
+    bool first, second, third;
+    Color c;
+
+    [Header("Bools")]
+    public bool drifting;
+
+    [Header("Parameters")]
+
+    public float acceleration = 30f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
+
+    [Header("Model Parts")]
+
+    public Transform frontWheels;
+    public Transform backWheels;
+    public Transform steeringWheel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float time = Time.timeScale == 1 ? .2f : 1;
+            Time.timeScale = time;
+        }
+
+        //Follow Collider
+        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+        //Accelerate
+        if (Input.GetButton("Fire1"))
+            speed = acceleration;
+
+        //Steer
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            
+        }
+
+        //Drift
+        if (Input.GetButtonDown("Jump") && !drifting && Input.GetAxis("Horizontal") != 0)
+        {
+            drifting = true;
+            driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+        }
+    }
+}
--- Crazy_Batlekarts/Assets/Scripts/RotacionDesierto.cs
+++ Crazy_Batlekarts/Assets/Scripts/RotacionDesierto.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class RotacionDesierto : MonoBehaviour
 {
-    public GameObject target;//the target object
-    public float speedMod = 10.0f;//a speed modifier
-    private Vector3 point;//the coord to the point where the camera looks at
+   /* public GameObject target;*///the target object
+  /*  public float speedMod = 10.0f;*///a speed modifier
+   /* private Vector3 point;*///the coord to the point where the camera looks at
 
     void Start()
     {//Set up things on the start method
-        point = target.transform.position;//get target's coords
-        transform.LookAt(point);//makes the camera look to it
+        /*point = target.transform.position;*///get target's coords
+       /* transform.LookAt(point)*/;//makes the camera look to it
     }
 
     void Update()
     {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
-        transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
+        //transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
     }
 }

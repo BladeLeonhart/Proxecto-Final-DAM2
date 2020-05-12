dad565291e0afcdeaf663216f2856626b0897ae7
Blade Leonhart <machosotoivan@gmail.com>
2020-05-12 18:15:18 +0200

    HealthBar e Script
    HealthBar engadida e scripts engadidos e arreglados. Faltan os enemigos e o checkpoint

--- Crazy_Batlekarts/Assets/Scripts/CamaraGuia.cs
+++ Crazy_Batlekarts/Assets/Scripts/CamaraGuia.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CamaraGuia : MonoBehaviour
 {
     public GameObject vehiculo;
-    private Vector3 desplazamiento = new Vector3(-4, 2 , 0);
+    private Vector3 desplazamiento = new Vector3(-4, 2, 0);
     // Start is called before the first frame update
     void Start()
     {

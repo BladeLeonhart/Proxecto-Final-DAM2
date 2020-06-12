9f3729227fe6a83770f587114e8b46e9a0f67314
kfr64 <kjfr.92@gmail.com>
2020-06-13 01:07:12 +0200

    Revert "Revert "Rebosque""
    This reverts commit 9ab4386ef1c732267cffe36b2b41db49ba92be90.

---
+++ Crazy_Batlekarts/Assets/Scripts/Desierto/Patrol.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] patrol;
+    private int Currentpoint;
+    public float moveSpeed;
+
+    void Start()
+    {
+        transform.position = patrol[0].position;
+        Currentpoint = 0;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, patrol[Currentpoint].position) < 0.5f)
+        {
+            Currentpoint++;
+        }
+
+        if (Currentpoint >= patrol.Length)
+        {
+            Currentpoint = 0;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, patrol[Currentpoint].position, moveSpeed * Time.deltaTime);
+    }
+}
